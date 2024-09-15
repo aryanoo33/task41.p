9e897df154bf9724f9786a79aff554bc71bb5acf
aryanoo33 <aryan4768.be23@chitkara.edu.in>
2024-09-15 17:17:01 +0530

    Add files via upload

---
+++ staffList.js
@@ -0,0 +1,33 @@
+import img1 from './1.jpeg';
+import img2 from './2.jpeg';
+import img3 from './3.jpeg';
+import { faker } from '@faker-js/faker'
+const staffList =[{
+    key:0,
+    avatar : img1,
+    name : 'Japan',
+    description : 'Japan is known for its rich cultural heritage, advanced technology, traditional arts',
+    star : '🌟5',
+    Author : 'Marco Polo'
+},
+{
+    key:1,
+    avatar : img2,
+    name : 'America',
+    description : "America is known for its cultural diversity, technological innovation, and iconic landmarks like the Statue of Liberty",
+    star : '🌟5',
+    Author : 'Alexis de Tocqueville'
+},
+{
+    key:2,
+    avatar : img3,
+    name : 'Australia',
+    description : "Australia is known for its unique wildlife, natural landscapes, rich indigenous culture",
+    star : '🌟5',
+    Author : 'Tim Winton'
+},
+
+
+
+]
+export default staffList
---
+++ tutList.js
@@ -0,0 +1,31 @@
+import img4 from './4.jpeg'
+import img5 from './5.jpeg'
+import img6 from './6.jpeg'
+
+const tutList = [{
+    key:0,
+    avatar : img4,
+    name : 'Jakarta',
+    description : 'Jakarta is known for its vibrant culture and historical landmarks',
+    star : '🌟4',
+    Author : 'Pramoedya'
+},
+{
+    key:1,
+    avatar : img5,
+    name : 'Russia',
+    description : "Russia is Known all over the world for its thinkers and artists",
+    star : '🌟5',
+    Author : 'Leo Tolstoy'
+},
+{
+    key:2,
+    avatar : img6,
+    name : 'France',
+    description : "Famous for contributing to fashion",
+    star : '🌟4',
+    Author : 'Vittorio Emanuele'
+},
+]
+
+export default tutList
